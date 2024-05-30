42a5b17d3328b9f2772613e3483b71e4debfeb35
CV funk <codygeary@users.noreply.github.com>
2024-05-30 14:40:26 +0200

    Signals refactor
    Refactoring the Signals code.

--- src/Signals.cpp
+++ src/Signals.cpp
@@ -11,7 +11,6 @@
 
 #include "plugin.hpp"
 #include "rack.hpp"
-
 #include <array>
 #include <vector>
 
@@ -19,7 +18,6 @@ using namespace rack;
 
 float MAX_TIME = 10.0f; // Max window time in seconds
 int MAX_BUFFER_SIZE; // Buffer size will be set in the constructor based on the sample rate
-
 
 struct Signals : Module {
     enum ParamId {
@@ -42,144 +40,129 @@ struct Signals : Module {
         NUM_LIGHTS
     };
 
-    //Declare global variables
-
     float currentTimeSetting = 1.0f;
     std::array<std::vector<float>, 6> envelopeBuffers;
-    std::array<int, 6> writeIndices = {}; // Track the current write position for each buffer
-    float lastInputs[5]={};
-
-    std::array<bool, 6> bufferCycledFlags = {false}; // Flags to indicate when each buffer has been cycled through
-    std::array<float, 6> lastTriggerTime; // Time since the last trigger for each channel
-    bool retriggerEnabled; 
+    std::array<int, 6> writeIndices = {}; 
+    float lastInputs[6] = {};
+    std::array<float, 6> lastTriggerTime = {}; 
+    bool retriggerEnabled = false; 
     bool retriggerToggleProcessed = false;
-    float forceRetriggerFlags[5]={};
+    std::array<bool, 6> forceRetriggerFlags = {};
+    double displayUpdateTime = 0.1; 
+    double timeSinceLastUpdate = 0.0;
 
+    FramebufferWidget* fbWidget = nullptr;
 
-    // Serialization method to save module state
     json_t* dataToJson() override {
         json_t* rootJ = json_object();
-
-        // Save the state of retriggerEnabled as a boolean
         json_object_set_new(rootJ, "retriggerEnabled", json_boolean(retriggerEnabled));
-
         return rootJ;
     }
 
-    // Deserialization method to load module state
     void dataFromJson(json_t* rootJ) override {
-        // Load the state of retriggerEnabled
         json_t* retriggerEnabledJ = json_object_get(rootJ, "retriggerEnabled");
         if (retriggerEnabledJ) {
-            // Use json_is_true() to check if the JSON value is true; otherwise, set to false
             retriggerEnabled = json_is_true(retriggerEnabledJ);
         }
     }
 
-
     Signals() {
         config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-        configParam(RANGE_PARAM, 0.0f, 1.0f, 0.5f, "Range");
-        configParam(TRIGGER_ON_PARAM, 0.f, 1.f, 1.f, "Retriggering"); // Toggle button
-        configParam(RANGE_BUTTON_PARAM, 0.f, 1.f, 0.f, "Range Mode"); // Switch
+        configParam(RANGE_PARAM, 0.1f, 0.9999f, 0.5f, "Range");
+        configParam(TRIGGER_ON_PARAM, 0.f, 1.f, 1.f, "Retriggering");
+        configParam(RANGE_BUTTON_PARAM, 0.f, 1.f, 0.f, "Range Mode");
 
-        lastTriggerTime.fill(0.0f); // Initialize the trigger timers
+        lastTriggerTime.fill(0.0f);
         MAX_BUFFER_SIZE = int(static_cast<int>(APP->engine->getSampleRate() * MAX_TIME));
 
         for (auto &buffer : envelopeBuffers) {
-            buffer.resize(MAX_BUFFER_SIZE, 0.0f); // Initialize buffers with zeros
+            buffer.resize(MAX_BUFFER_SIZE, 0.0f);
         }
     }
 
-    void process(const ProcessArgs& args) override {     
-        float range = params[RANGE_PARAM].getValue(); // Range knob value [0, 1]
+    void process(const ProcessArgs& args) override {
+        float range = pow(params[RANGE_PARAM].getValue(),3.0f);
 
-        //Set the Time Range by switch
-        if (params[RANGE_BUTTON_PARAM].getValue()>0.5) {
-            range *= 0.89;//wonky fix for range issue
+        if (params[RANGE_BUTTON_PARAM].getValue() > 0.5) {
             currentTimeSetting = MAX_TIME;
-            lights[LONG_LIGHT].setBrightness( 1.0f );
+            lights[LONG_LIGHT].setBrightness(1.0f);
         } else {
             currentTimeSetting = 1.0f;
-            lights[LONG_LIGHT].setBrightness( 0.0f );
+            lights[LONG_LIGHT].setBrightness(0.0f);
         }
+        range = clamp(range, 0.000001f, .9999f);
 
-        range = clamp(range, 0.000001f, .9999f); //avoid artifact at end of range
+        int currentBufferSize = int((MAX_BUFFER_SIZE / MAX_TIME) * currentTimeSetting * range);
 
-        for (int i = 0; i < NUM_INPUTS; ++i) {
+        for (int i = 0; i < 6; ++i) { //for the 6 wave inputs
             if (inputs[i].isConnected()) {
-                float inputVoltage = clamp (inputs[i].getVoltage(), -10.f, 10.f);
+                float inputVoltage = clamp(inputs[i].getVoltage(), -10.f, 10.f);
 
-                // Force a retrigger for this channel if the flag is set
                 if (forceRetriggerFlags[i]) {
                     std::fill(envelopeBuffers[i].begin(), envelopeBuffers[i].end(), 0.0f);
                     writeIndices[i] = 0;
-                    lastTriggerTime[i] = 0.0f; // Reset the timer for this channel
-                    forceRetriggerFlags[i] = false; // Clear the flag after retriggering
+                    lastTriggerTime[i] = 0.0f;
+                    forceRetriggerFlags[i] = false;
                 }
-                            
-                // Update the timer for this channel
+
                 lastTriggerTime[i] += args.sampleTime;
 
-                // Check for retrigger condition only if retriggering is enabled
                 if (retriggerEnabled && inputVoltage > 1.0f 
                     && lastInputs[i] <= 1.0f 
-                    && lastTriggerTime[i] >= (range*.99f*currentTimeSetting + .01f) ) {
-                
-                    //    std::fill(envelopeBuffers[i].begin(), envelopeBuffers[i].end(), 0.0f);
+                    && lastTriggerTime[i] >= ( range  * currentTimeSetting ) ) {
                         writeIndices[i] = 0;
-                        lastTriggerTime[i] = 0.0f; // Reset the timer
-                        forceRetriggerFlags[i] = false; // Clear the flag after retriggering
+                        lastTriggerTime[i] = 0.0f;
+                        forceRetriggerFlags[i] = false;
                 } else {
-                    // Continue writing to buffer if not triggered or if retriggering is disabled
                     envelopeBuffers[i][writeIndices[i]] = inputVoltage;
-                    writeIndices[i] = (writeIndices[i] + 1) % int((MAX_BUFFER_SIZE/MAX_TIME)*currentTimeSetting);
+                    writeIndices[i] = (writeIndices[i] + 1) % currentBufferSize;
                 }
 
                 lastInputs[i] = inputVoltage;
             } else if (lastInputs[i] != 0.0f) {
-                // Input was previously connected but now is not, reset the buffer once and set last input to 0
                 std::fill(envelopeBuffers[i].begin(), envelopeBuffers[i].end(), 0.0f);
                 writeIndices[i] = 0;
-                lastInputs[i] = 0.0f; // Update lastInputs to indicate the channel is now inactive
-                lastTriggerTime[i] = 0.0f; // Reset the timer
+                lastInputs[i] = 0.0f;
+                lastTriggerTime[i] = 0.0f;
             }
-            // If the input is not connected and last input is already 0, do nothing
-        }//for(int i=0...
+        }
 
-        // Toggle retriggerEnabled state when the button is pressed
         if (params[TRIGGER_ON_PARAM].getValue() > 0.5f && !retriggerToggleProcessed) {
             retriggerEnabled = !retriggerEnabled;
-            retriggerToggleProcessed = true; // Mark that we've processed this toggle
-            params[TRIGGER_ON_PARAM].setValue(0.0f); // Reset the button's parameter value to simulate a latch
+            retriggerToggleProcessed = true;
+            params[TRIGGER_ON_PARAM].setValue(0.0f);
 
-            // If retriggering is now disabled, sync all channels
             if (!retriggerEnabled) {
                 for (int i = 0; i < NUM_INPUTS; ++i) {
                     std::fill(envelopeBuffers[i].begin(), envelopeBuffers[i].end(), 0.0f);
                     writeIndices[i] = 0;
-                    lastTriggerTime[i] = 0.0f; // Reset the timer for each channel
+                    lastTriggerTime[i] = 0.0f;
                 }
             }
         } else if (params[TRIGGER_ON_PARAM].getValue() <= 0.5f) {
-            retriggerToggleProcessed = false; // Allow for another toggle when the button is released and pressed again
+            retriggerToggleProcessed = false;
         }
 
-        // Set light brightness based on the toggle state
         lights[TRIGGER_ON_LIGHT].setBrightness(retriggerEnabled ? 1.0f : 0.0f);
 
-    
         for (int i = 0; i < NUM_INPUTS; ++i) {
             if (inputs[i].isConnected()) {
-                // Directly pass through the input to the associated output
                 outputs[i].setVoltage(inputs[i].getVoltage());
             } else {
                 outputs[i].setVoltage(0.0f);
             }
         }
-                
-    }//void
-};//module
+
+        timeSinceLastUpdate += args.sampleTime;
+        if (timeSinceLastUpdate >= displayUpdateTime) {
+            timeSinceLastUpdate = 0.0;
+            if (fbWidget) {
+                fbWidget->dirty = true;
+            }
+        }
+    }
+};
+
 
 struct WaveformDisplay : TransparentWidget {
     Signals* module;
@@ -192,12 +175,7 @@ struct WaveformDisplay : TransparentWidget {
         if (!module) return;
 
         const auto& buffer = module->envelopeBuffers[channelId];
-        float range = module->params[Signals::RANGE_PARAM].getValue() / (MAX_TIME / module->currentTimeSetting);
-
-        // Adjust the Time Range if the range button is toggled
-        if (module->params[Signals::RANGE_BUTTON_PARAM].getValue() > 0.5) {
-            range *= 0.89; // Scale adjustment due to precision issues at slow scale
-        } 
+        float range = pow(module->params[Signals::RANGE_PARAM].getValue(),3.0f) / (MAX_TIME / module->currentTimeSetting);
 
         int displaySamples = 1024;
         std::vector<Vec> points;
@@ -220,20 +198,14 @@ struct WaveformDisplay : TransparentWidget {
             points.push_back(Vec(x, y));
         }
 
+        //draw the wave
         nvgBeginPath(args.vg);
-        nvgStrokeWidth(args.vg, range + 1.5);
-        nvgStrokeColor(args.vg, waveformColor);
-
-        if (module->retriggerEnabled) {
-            nvgMoveTo(args.vg, 0, box.size.y);
-        } else {
-            nvgMoveTo(args.vg, points[0].x, points[0].y);
-        }
-
-        for (size_t i = 1; i < points.size(); ++i) {
+        nvgStrokeWidth(args.vg, -module->params[Signals::RANGE_PARAM].getValue() + 1.5);
+        nvgStrokeColor(args.vg, waveformColor);        
+        nvgMoveTo(args.vg, points[0].x, points[0].y);
+        for (size_t i = 0; i < points.size()-1; ++i) {
             nvgLineTo(args.vg, points[i].x, points[i].y);
         }
-
         nvgStroke(args.vg);
     }
 
@@ -245,13 +217,13 @@ struct WaveformDisplay : TransparentWidget {
     }
 
     void draw(const DrawArgs& args) override {
-        //Only drawing in the self-illuminating layer
+        // Only drawing in the self-illuminating layer
     }
 };
 
-
-
 struct SignalsWidget : ModuleWidget {
+    FramebufferWidget* fbWidget;
+
     SignalsWidget(Signals* module) {
         setModule(module);
               
@@ -266,13 +238,20 @@ struct SignalsWidget : ModuleWidget {
         addChild(createWidget<ThemedScrew>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
         // Range knob
-        addParam(createParam<RoundBlackKnob>(mm2px(Vec(5, 14)), module, Signals::RANGE_PARAM));
+        addParam(createParam<RoundBlackKnob>(mm2px(Vec(5, 12)), module, Signals::RANGE_PARAM));
 
-        addParam(createParam<CKSS>(mm2px(Vec(22, 17)), module, Signals::RANGE_BUTTON_PARAM));
-        addChild(createLightCentered<SmallLight<RedLight>>(mm2px(Vec(28, 19)), module, Signals::LONG_LIGHT));
+        addParam(createParam<CKSS>(mm2px(Vec(17, 14)), module, Signals::RANGE_BUTTON_PARAM));
+        addChild(createLightCentered<SmallLight<RedLight>>(mm2px(Vec(23, 16)), module, Signals::LONG_LIGHT));
 
-        addParam(createParamCentered<TL1105>(mm2px(Vec(38, 19)), module, Signals::TRIGGER_ON_PARAM));
-        addChild(createLightCentered<SmallLight<RedLight>>(mm2px(Vec(44, 19)), module, Signals::TRIGGER_ON_LIGHT));
+        addParam(createParamCentered<TL1105>(mm2px(Vec(50, 19)), module, Signals::TRIGGER_ON_PARAM));
+        addChild(createLightCentered<SmallLight<RedLight>>(mm2px(Vec(55, 19)), module, Signals::TRIGGER_ON_LIGHT));
+
+        fbWidget = new FramebufferWidget();
+        addChild(fbWidget);
+
+        if (module) {
+            module->fbWidget = fbWidget;
+        }
 
         NVGcolor colors[6] = {
             nvgRGB(0xa0, 0xa0, 0xa0), // Even Lighter Grey
@@ -288,18 +267,17 @@ struct SignalsWidget : ModuleWidget {
         for (int i = 0; i < 6; ++i) {
             float yPos = initialYPos + i * spacing; // Adjusted positioning and spacing
 
-            addInput(createInput<ThemedPJ301MPort>(Vec(5, yPos+20), module, i));
-            addOutput(createOutput<ThemedPJ301MPort>(Vec(148, yPos+20), module, i));
+            addInput(createInput<ThemedPJ301MPort>(Vec(5, yPos + 20), module, i));
+            addOutput(createOutput<ThemedPJ301MPort>(Vec(148, yPos + 20), module, i));
 
             WaveformDisplay* display = new WaveformDisplay(colors[i]);
-            display->box.pos = Vec(40, yPos );
-            display->box.size = Vec(100, 40); 
+            display->box.pos = Vec(39, yPos);
+            display->box.size = Vec(104, 40); 
             display->module = module;
             display->channelId = i;
-            addChild(display);
+            fbWidget->addChild(display);
         }
-
     }
-    
 };
+
 Model* modelSignals = createModel<Signals, SignalsWidget>("Signals");
