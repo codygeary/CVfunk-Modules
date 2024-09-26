2eb4a210e6c788b5e2fe01956de6b07f2f9914ae
CV funk <codygeary@users.noreply.github.com>
2024-09-27 00:05:34 +0200

    Add Arrange module

---
+++ plugin.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <rack.hpp>
+
+
+using namespace rack;
+
+// Declare the Plugin, defined in plugin.cpp
+extern Plugin* pluginInstance;
+
+// Declare each Model, defined in each module source file
+extern Model* modelSteps;
+extern Model* modelEnvelopeArray;
+extern Model* modelPentaSequencer;
+extern Model* modelImpulseController;
+extern Model* modelSignals;
+extern Model* modelRanges;
+extern Model* modelHexMod;
+extern Model* modelCollatz;
+extern Model* modelStrings;
+extern Model* modelMagnets;
+extern Model* modelOuros;
+extern Model* modelPressedDuck;
+extern Model* modelFlowerPatch;
+extern Model* modelSyncro;
+extern Model* modelNona;
+extern Model* modelDecima;
+extern Model* modelMorta;
+extern Model* modelStepWave;
+extern Model* modelPreeeeeeeeeeessedDuck;
+extern Model* modelArrange;
+
+
